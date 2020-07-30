0ed1994d08eef61d8ba2437a06b548d78a1fc8d8
Hexagons <anton.heestand@gmail.com>
2020-07-30 18:24:59 +0200

    padding

--- Sources/MultiplatformTypes/SwiftUI/Color.swift
+++ Sources/MultiplatformTypes/SwiftUI/Color.swift
@@ -1,5 +1,5 @@
 import SwiftUI
 
 public extension Color {
-    static let clearGray: Color = Color(.displayP3, white: 0.5, opacity: 0.123 /*0.001*/)
+    static let clearGray: Color = Color(.displayP3, white: 0.5, opacity: 0.001)
 }
