cfa3b9f2aa289c908dc4a0253681b3aaa13f2412
Anton ⬢ Hexagons <anton.heestand@gmail.com>
2020-08-07 11:43:15 +0200

    background color

--- Sources/MultiplatformTypes/SwiftUI/Color.swift
+++ Sources/MultiplatformTypes/SwiftUI/Color.swift
@@ -3,3 +3,13 @@ import SwiftUI
 public extension Color {
     static let clearGray: Color = Color(.displayP3, white: 0.5, opacity: 0.001)
 }
+
+public extension Color {
+    static func background(colorScheme: ColorScheme) -> Color {
+        #if os(macOS)
+        return Color(.windowBackgroundColor)
+        #else
+        return colorScheme == .light ? .white : .black
+        #endif
+    }
+}
