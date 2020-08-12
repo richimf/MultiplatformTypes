cba9b9d735939f465f21d1b77eb064746a19c9d7
Anton ⬢ Hexagons <anton.heestand@gmail.com>
2020-08-12 18:50:04 +0200

    Interact

---
+++ Sources/MultiplatformTypes/SwiftUI/InteractView.swift
@@ -0,0 +1,65 @@
+import SwiftUI
+
+@available(*, deprecated, renamed: "InteractView")
+typealias TouchView = InteractView
+
+public struct InteractView: ViewRepresentable {
+    
+    let interacted: (Bool) -> ()
+    
+    public init(_ interacted: @escaping (Bool) -> ()) {
+        self.interacted = interacted
+    }
+    
+    public func makeView(context: Context) -> MPTouchView {
+        MPTouchView(interacted: interacted)
+    }
+    
+    public func updateView(_ view: MPTouchView, context: Context) {}
+    
+}
+
+public class MPTouchView: MPView {
+    
+    let interacted: (Bool) -> ()
+    
+    init(interacted: @escaping (Bool) -> ()) {
+        self.interacted = interacted
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    #if os(iOS)
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        interacted(true)
+    }
+    public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        interacted(false)
+    }
+    public override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        interacted(false)
+    }
+    
+    #elseif os(macOS)
+
+    public override func mouseDown(with event: NSEvent) {
+        interacted(true)
+    }
+    
+    public override func mouseUp(with event: NSEvent) {
+        interacted(false)
+    }
+    
+    #endif
+    
+}
+
+struct InteractView_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
