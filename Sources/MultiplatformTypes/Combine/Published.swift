c8804c9c82248d2e531a4f14c203ee73d20dc018
Anton ⬢ Hexagons <anton.heestand@gmail.com>
2020-08-18 23:05:30 +0200

    VJ with Love

---
+++ Sources/MultiplatformTypes/Combine/Published.swift
@@ -0,0 +1,22 @@
+import Foundation
+import Combine
+
+class PublishedStorage {
+    static var storage: Set<AnyCancellable> = []
+}
+
+public extension Published {
+    
+    init(wrappedValue: Value, key: String) {
+        var value: Value = UserDefaults.standard.object(forKey: key) as? Value ?? wrappedValue
+        self.init(wrappedValue: value)
+        self.projectedValue.sink { value in
+            UserDefaults.standard.set(value, forKey: key)
+        }
+        .store(in: &PublishedStorage.storage)
+    }
+    
+}
+
+
+
